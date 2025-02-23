836d92c563b5c57079fa4721d137728ee16906aa
clchiu <clchiu@nvidia.com>
2025-02-23 18:42:34 +0800

    Add fine-tuning script for Slurm cluster

---
+++ Slurm/run_finetuning.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ml slurm
+
+NEMO_PATH=${PWD}/NeMo
+
+PARTITION=defq
+CONTAINER=nvcr.io/nvidia/nemo:dev
+JOB_NAME=llama31_finetuning
+
+NUM_NODES=1
+NUM_GPUS=8
+
+HF_MODEL_ID=meta-llama/Llama-3.1-8B-Instruct
+NEMO_MODEL=
+
+HF_TOKEN=<HF_TOKEN>
+
+MAX_STEPS=100
+GBS=128
+TP=2
+PP=1
+CP=1
+
+DATASET_PATH=data/alpaca
+
+experiment_id=$(srun -p ${PARTITION} -G 8 \
+--container-image $CONTAINER \
+--container-mounts ${NEMO_PATH}:${NEMO_PATH} \
+--container-workdir ${NEMO_PATH} \
+--container-writable \
+--no-container-mount-home \
+bash -c \
+"python finetune.py \
+    --executor slurm \
+    --account ${USER} \
+    --partition ${PARTITION} \
+    --container_image ${CONTAINER} \
+    --experiment ${JOB_NAME} \
+    --num_nodes ${NUM_NODES} \
+    --num_gpus ${NUM_GPUS} \
+    --hf_model_id ${HF_MODEL_ID} \
+    --nemo_model ${NEMO_MODEL} \
+    --hf_token ${HF_TOKEN} \
+    --max_steps ${MAX_STEPS} \
+    --global_batch_size ${GBS} \
+    --tensor_model_parallel_size ${TP} \
+    --pipeline_model_parallel_size ${PP} \
+    --context_parallel_size ${CP} \
+    --dataset_path ${DATASET_PATH}" | grep "Experiment" | sed -n 's/.*\[[^]]*\] Experiment \([^ ]*\).*/\1/p')
+
+echo "Your NeMo Experiment is submitted with job name: ${experiment_id}"
+sbatch --requeue ${NEMO_PATH}/experiments/${JOB_NAME}/${experiment_id}/finetuning_sbatch.sh
