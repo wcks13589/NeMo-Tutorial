d0cb869ced5175d4d75733c5a8723f47b1a4ed8e
clchiu <clchiu@nvidia.com>
2025-04-18 14:41:47 +0800

    Add dryrun args

--- finetuning/finetune.py
+++ finetuning/finetune.py
@@ -181,7 +181,7 @@ def run_finetuning(args):
 
     with run.Experiment(args.experiment, base_dir=WORK_PATH) as exp:
         exp.add(recipe, executor=executor, name="finetuning")
-        exp.dryrun(delete_exp_dir=False) if args.executor == "slurm" else exp.run(sequential=True, tail_logs=True)
+        exp.dryrun(delete_exp_dir=False) if args.executor == "slurm" or args.dryrun else exp.run(sequential=True, tail_logs=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="NeMo Finetuning Arguments")
